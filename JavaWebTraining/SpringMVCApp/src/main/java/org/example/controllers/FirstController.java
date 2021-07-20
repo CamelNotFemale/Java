66f08210219864c3b251222b534f075bb6282115
boris_nem <58486153+CamelNotFemale@users.noreply.github.com>
2021-07-20 19:40:36 +0300

    @Controller and @RequestMapping

---
+++ JavaWebTraining/SpringMVCApp/src/main/java/org/example/controllers/FirstController.java
@@ -0,0 +1,21 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first") // теперь все методы Контроллера доступны по URL: /first/...
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+
+}
