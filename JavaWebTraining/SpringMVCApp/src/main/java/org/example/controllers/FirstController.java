1fcb92747ba4ed2253a9a128e70a322867dd2a88
boris_nem <58486153+CamelNotFemale@users.noreply.github.com>
2021-07-21 16:10:52 +0300

    acquaintance with the concept of a Model in the MVC scheme, creating a simple calculator "/first/calculator"

--- JavaWebTraining/SpringMVCApp/src/main/java/org/example/controllers/FirstController.java
+++ JavaWebTraining/SpringMVCApp/src/main/java/org/example/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,11 +24,41 @@ public class FirstController {
 
     @GetMapping("/goodbye")
     // второй способ получения параметров GET (получение исключительно нужных параметров)
-    // (!!!) в случае передачи пустых параметров - возвращает код 404
+    // (!!!) для @RequestParam("name")в случае передачи пустых параметров - возвращает код 404
     // для избежания такой проблемы можно использовать @RequestParam(value = "name", required = false)
-    public String goodByePage(@RequestParam("name") String name) {
-        System.out.println("Goodbye " + name);
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                              Model model) {
+        //System.out.println("Goodbye " + name);
+        // передаём в Модель новый параметр, который может исопльзовать Отображение
+        model.addAttribute("message", "Goodbye " + name);
         return "first/goodbye";
     }
 
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("action") String action,
+                            @RequestParam("a") int a,
+                            @RequestParam("b") int b,
+                              Model model) {
+        String res = null;
+        // выбираем арифметическое действие
+        switch (action) {
+            case "multiplication":
+                res = a + " * " + b + " = " + a*b;
+                break;
+            case "addition":
+                res = a + " + " + b + " = " + (a+b);
+                break;
+            case "subtraction":
+                res = a + " - " + b + " = " + (a-b);
+                break;
+            case "division":
+                res = a + " / " + b + " = " + a/b;
+                break;
+        }
+        // передаём в Модель новый параметр, который может исопльзовать Отображение
+        model.addAttribute("action", action);
+        model.addAttribute("result", res);
+
+        return "first/calculator";
+    }
 }
