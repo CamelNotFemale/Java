5c9c9695de07b3322c0367c7565f91e849ad9771
boris_nem <58486153+CamelNotFemale@users.noreply.github.com>
2021-07-21 15:31:17 +0300

    familiarization with method GET(HTTP) and @RequestParam(Spring)

--- JavaWebTraining/SpringMVCApp/src/main/java/org/example/controllers/FirstController.java
+++ JavaWebTraining/SpringMVCApp/src/main/java/org/example/controllers/FirstController.java
@@ -3,18 +3,30 @@ package org.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first") // теперь все методы Контроллера доступны по URL: /first/...
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    // первый способ получения параметров GET
+    // (!!!) в случае передачи пустых параметров - присваивает им значения null
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        System.out.println("Hello " + name);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    // второй способ получения параметров GET (получение исключительно нужных параметров)
+    // (!!!) в случае передачи пустых параметров - возвращает код 404
+    // для избежания такой проблемы можно использовать @RequestParam(value = "name", required = false)
+    public String goodByePage(@RequestParam("name") String name) {
+        System.out.println("Goodbye " + name);
         return "first/goodbye";
     }
 
